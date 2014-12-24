fab57f3f2fd2a1a41695c328d465730deb0c6f93
Jörg Weber <weber.joerg.h@gmail.com>
2014-12-25 00:22:02 +0100

    Foosball Game Schema
    * define a schema
    * add methods
    * add specs

--- server/api/foosball-game/foosball-game.model.js
+++ server/api/foosball-game/foosball-game.model.js
@@ -1,12 +1,97 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  Schema = mongoose.Schema,
+  FoosballGameRound = require('./foosball-game-round.model'),
+  User = require('../user/user.model'),
+  _ = require('lodash'),
+  Reader = require('../../components/qr/qr'),
+  streamService = require('../../stream');
+
+function FoosballGameModelError(message) {
+  this.name = 'FoosballGameModelError';
+  this.message = (message || '');
+}
+FoosballGameModelError.prototype = Error.prototype;
 
 var FoosballGameSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  player: [{type: Schema.Types.ObjectId, ref: 'User'}],
+  rounds: [{type: Schema.Types.ObjectId, ref: 'FoosballGameRound'}],
+  active: {type: Boolean, default: false},
+  finished: {type: Boolean, default: false}
 });
+
+FoosballGameSchema.methods = {
+  addPlayer: function (user, cb) {
+    var game = this, error = null;
+    if (!(user instanceof User)) {
+      error = new FoosballGameModelError(user + ' is not an instance of ‘User’');
+      return cb(error);
+    }
+
+    if (this.player.length === 4) {
+      error = new FoosballGameModelError('Already 4 player assigned to the game.');
+      return cb(error);
+    }
+
+    this.player.push(user._id);
+    this.save(function (error) {
+      cb(error, game);
+    });
+  },
+
+  startNewRound: function (roundNr, cb) {
+    var game = this;
+
+    this.readPlayerFromQr(function(error, player){
+      if(error){
+        return cb(error);
+      }
+
+      FoosballGameRound.create({
+        nr: roundNr,
+        game: game._id,
+        red_offense: player.red_offense,
+        red_defense: player.red_defense,
+        blue_offense: player.blue_offense,
+        blue_defense: player.blue_defense
+      }, function (error, round) {
+        if (error) {
+          return cb(error)
+        }
+        game.rounds.push(round);
+        game.save(function (error) {
+          cb(error);
+        });
+
+      });
+    });
+  },
+
+  readPlayerFromQr: function (cb) {
+    var player = {},
+      qr = new Reader();
+    var readerCallback = function (playerName, x, y, playerColor, playerPosition) {
+      User.findOne({name: playerName}).exec(function (error, user) {
+        if (error) {
+          return cb(error);
+        }
+
+        player[playerColor + '_' + playerPosition] = user._id;
+
+        if (_.size(player) === 4) {
+          cb(null, player);
+        }
+      });
+    };
+
+    streamService.getQrCodeImagePath(function (error, imagePath) {
+      if (error) {
+        return cb(error)
+      }
+      qr.loadImage(imagePath, readerCallback);
+    });
+  }
+};
 
 module.exports = mongoose.model('FoosballGame', FoosballGameSchema);
--- server/api/foosball-game/foosball-game.spec.js
+++ server/api/foosball-game/foosball-game.spec.js
@@ -1,20 +1,186 @@
 'use strict';
 
-var should = require('should');
-var app = require('../../app');
-var request = require('supertest');
+var should = require('should'),
+  path = require('path'),
+  app = require('../../app'),
+  request = require('supertest'),
+  FoosballGame = require('./foosball-game.model'),
+  FoosballGameRound = require('./foosball-game-round.model'),
+  User = require('../user/user.model'),
+  user1 = {
+    provider: 'local',
+    name: 'Slava',
+    email: 'user1@test.com',
+    password: 'password'
+  },
+  user2 = {
+    provider: 'local',
+    name: 'mascha',
+    email: 'user2@test.com',
+    password: 'password'
+  },
+  user3 = {
+    provider: 'local',
+    name: '3Dfx',
+    email: 'user3@test.com',
+    password: 'password'
+  },
+  user4 = {
+    provider: 'local',
+    name: 'TwerkJörg',
+    email: 'user4@test.com',
+    password: 'password'
+  };
 
-describe('GET /api/foosball-games', function() {
-
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/foosball-games')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
+describe('Foosball game model', function () {
+  before(function (done) {
+    // Clear games before testing
+    FoosballGame.remove().exec().then(function () {
+      FoosballGameRound.remove().exec().then(function () {
+        User.remove().exec().then(function () {
+          done();
+        });
       });
+    });
+  });
+
+  afterEach(function (done) {
+    FoosballGame.remove().exec().then(function () {
+      FoosballGameRound.remove().exec().then(function () {
+        User.remove().exec().then(function () {
+          done();
+        });
+      });
+    });
+  });
+
+  it('should begin with no games', function (done) {
+    FoosballGame.find({}, function (err, games) {
+      games.should.have.length(0);
+      done();
+    });
+  });
+
+  describe('GET /api/foosball-games', function () {
+
+    it('should respond with JSON array', function (done) {
+      request(app)
+        .get('/api/foosball-games')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function (err, res) {
+          if (err) return done(err);
+          res.body.should.be.instanceof(Array);
+          done();
+        });
+    });
+  });
+
+
+  describe('CRUD', function () {
+
+    it('should create a game with defaults', function (done) {
+      FoosballGame.create({}, function (error, game) {
+        should.not.exist(error);
+        //noinspection BadExpressionStatementJS
+        game.finished.should.be.an.instanceOf(Boolean).and.be.false;
+        //noinspection BadExpressionStatementJS
+        game.active.should.be.an.instanceOf(Boolean).and.be.false;
+        done();
+      })
+    });
+
+  });
+
+  describe('Methods', function () {
+
+    describe('.addPlayer', function(){
+      it('should return error if user is not an instance of User', function (done) {
+        var player = 'user';
+
+        FoosballGame.create({}, function (error, game) {
+          game.addPlayer(player, function (error) {
+            should.exist(error);
+            error.message.should.equal(player + ' is not an instance of ‘User’');
+            done();
+          });
+        })
+      });
+
+      it('should return error if list of player already contains 4', function (done) {
+        var player1 = new User(user1),
+          player2 = new User(user2),
+          player3 = new User(user3),
+          player4 = new User(user4);
+
+        FoosballGame.create({
+          player: [player1._id, player2._id, player3._id, player4._id]
+        }, function (error, game) {
+          game.addPlayer(player1, function (error) {
+            should.exist(error);
+            error.message.should.equal('Already 4 player assigned to the game.');
+            done();
+          });
+        })
+      });
+
+      it('should add a player to player list', function (done) {
+        var player = new User(user1);
+
+        FoosballGame.create({}, function (error, game) {
+          game.addPlayer(player, function (error, game) {
+            should.not.exist(error);
+            FoosballGame.findById(game._id).exec(function (error, game) {
+              should.not.exist(error);
+              game.player.should.have.length(1);
+              done();
+            })
+          });
+        })
+      });
+    });
+
+    describe('.readPlayerFromQr', function(){
+
+      it('should return list of player with correct position and color', function (done) {
+        this.timeout('20000');
+
+        User.create([user1, user2, user3, user4], function(){
+          FoosballGame.create({}, function (error, game) {
+            game.readPlayerFromQr(function (error) {
+              should.not.exist(error);
+              done();
+            });
+          });
+        });
+
+      });
+
+    });
+
+    describe('.startNewRound', function(){
+      it('should add a new round to the game', function(done){
+        this.timeout('20000');
+
+        User.create([user1, user2, user3, user4], function(){
+          FoosballGame.create({}, function (error, game) {
+            game.rounds.should.have.length(0);
+
+            game.startNewRound(1, function (error) {
+              should.not.exist(error);
+
+              FoosballGame.findOne().exec(function(error, game){
+
+                game.rounds.should.have.length(1);
+                done();
+
+              });
+            });
+          });
+        });
+      });
+
+    });
+
   });
 });
