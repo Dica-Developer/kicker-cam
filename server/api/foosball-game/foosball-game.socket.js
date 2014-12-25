949336f7be6e3e9b43875c9228427cb85510e92a
Jörg Weber <weber.joerg.h@gmail.com>
2014-12-25 12:47:28 +0100

    * populate game rounds and player before sending via socket

--- server/api/foosball-game/foosball-game.socket.js
+++ server/api/foosball-game/foosball-game.socket.js
@@ -8,12 +8,22 @@ var FoosballGame = require('./foosball-game.model');
 
 exports.register = function(socket) {
   FoosballGame.schema.post('save', function (doc) {
-    onSave(socket, doc);
+    var populateOptions = [
+      {path: 'player', select: 'name', model: 'User'},
+      {path: 'rounds', model: 'FoosballGameRound'},
+      {path: 'rounds.red_offense', select: 'name', model: 'User'},
+      {path: 'rounds.red_defense', select: 'name', model: 'User'},
+      {path: 'rounds.blue_offense', select: 'name', model: 'User'},
+      {path: 'rounds.blue_defense', select: 'name', model: 'User'}
+    ];
+    FoosballGame.populate(doc, populateOptions, function(err, doc){
+      onSave(socket, doc);
+    });
   });
   FoosballGame.schema.post('remove', function (doc) {
     onRemove(socket, doc);
   });
-}
+};
 
 function onSave(socket, doc, cb) {
   socket.emit('foosball-game:save', doc);
